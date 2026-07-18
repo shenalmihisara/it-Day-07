106662b93cbf337781ef0036f584ad435300713f
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-18 13:06:22 +0530

    Refactor: Comment out unused product and function definitions in app.js

--- app.js
+++ app.js
@@ -11,12 +11,12 @@
 // console.log(revCustomerList);
 
 
-const products = [
-    {name: "bun", inStock: true},
-    {name: "car", inStock: false},
-    {name: "bat", inStock: true},
-    {name: "van", inStock: false}
-]
+// const products = [
+//     {name: "bun", inStock: true},
+//     {name: "car", inStock: false},
+//     {name: "bat", inStock: true},
+//     {name: "van", inStock: false}
+// ]
 
 
 // let inStockItems = products.filter(
@@ -31,3 +31,60 @@ const products = [
 
 // console.log(inStockItems);
 
+// step 01 ---------------------
+// function getSum(num1,num2) {
+//     return num1+num2
+// }
+
+// getSum(10,20);
+
+//step  02----------------------
+
+// let getSum = function(num1,num2){
+//     return num1+num2;
+// }
+// console.log(getSum(10,20));
+
+//step 03-----------------------
+
+// let getSum = (num1,num2) => {
+//     return num1+num2;
+// }
+// console.log(getSum(10,20));
+
+//step 04-------------------------
+
+
+// let sample = textValue => {
+//     return textValue;
+// }
+// console.log(sample("hi saman"));
+
+//step 05--------------------------
+
+
+// let getSum = (num1, num2) => num1 + num2;
+
+// console.log(getSum(10,20));
+
+
+// step 06---------------------------
+
+// let sample = txtValue => txtValue;
+
+// console.log("hi saman");
+
+// let inStockItems = products.filter(product => product.inStock == false);
+
+const productsList = [
+    {name: "bun", inStock: true},
+    {name: "car", inStock: false},
+    {name: "bat", inStock: true},
+    {name: "van", inStock: false}
+]
+
+// for(product of productsList){
+//     console.log(product);
+// }
+
+productsList.forEach(product => console.log(product));
