cf5afdacfb2cf51ea3102464b26055d61934eaf2
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-18 16:10:01 +0530

    Add fetch functionality and customer data JSON file
    - Implemented fetch request to retrieve and display todo items from an external API.
    - Added a new customer.json file containing customer data.
    - Updated index.html to include a table for displaying the fetched todo list.

--- app.js
+++ app.js
@@ -111,3 +111,30 @@
 // let student = studentList.find(student => student.id==="003")
 
 // console.log(student);
+
+// fetch("/customer.json").then(res=>res.json()).then(data=>{
+//     console.log(data);
+    
+// })
+
+fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
+    console.log(data);
+
+    let tblTodoList = document.getElementById("tblTodoList");
+
+    let body=""
+
+    data.forEach(element => {
+        body+= `
+                <tr>
+            <td>${element.id}</td>
+            <td>${element.title}</td>
+            <td>${element.userId}</td>
+            <td>${element.completed}</td>
+        </tr>
+        `
+    });
+
+    tblTodoList.innerHTML=body;
+
+})
