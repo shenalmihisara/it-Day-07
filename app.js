c0bd81bf4e3dfeff207b9b3f84a6b2b50d2cb3d6
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-18 12:33:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// const customerList=[];
+
+// customerList.push(1);
+// customerList.push(2);
+// customerList.push(3);
+// customerList.push(4);
+
+// console.log(customerList);
+
+// let revCustomerList=customerList.reverse();
+// console.log(revCustomerList);
+
+
+const products = [
+    {name: "bun", inStock: true},
+    {name: "car", inStock: false},
+    {name: "bat", inStock: true},
+    {name: "van", inStock: false}
+]
+
+
+// let inStockItems = products.filter(
+//     function(products) {
+//         return productFilter(product)
+//     }
+// ) 
+
+// function productFilter(product){
+//     return product.inStock == true;
+// }
+
+// console.log(inStockItems);
+
